094fca5400ede0ea1a88a469377d2fda3844e6c5
veit <veit@blueshoe.de>
2021-01-01 17:14:56 +0100

    remove cp traces

--- tests/testapp/urls.py
+++ tests/testapp/urls.py
@@ -1,11 +1,7 @@
-from django.http import HttpResponse
+from django.contrib.admin import site
 from django.urls import path
 
 
-def test_view(request):
-    return HttpResponse("Hello world!", status=200)
-
-
 urlpatterns = [
-    path("", test_view),
+    path("admin/", site.urls),
 ]
